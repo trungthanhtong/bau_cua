e0fce54ec01b6732fb6d14059955031322a2fa07
Thanh Tong <47911618+trungthanhtong@users.noreply.github.com>
2021-02-09 07:16:25 -0800

    Update rootReducer.js

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -1,6 +1,4 @@
 import {combineReducers} from 'redux'
-import ToDoListReducer from './ToDoListReducer'
-import FakebookReducer from './FakebookReducer'
 import GameBauCuaReducer from './BaiTapGameBauCuaReducer'
 
 export const rootReducer = combineReducers({

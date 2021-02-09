bff4ffd7685c70887e297da2de45ce435ffba301
Thanh Tong <47911618+trungthanhtong@users.noreply.github.com>
2021-02-09 07:17:32 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,50 +1,9 @@
-import UserProfile from "./FormValidation/UserProfile/UserProfile";
-import DemoHookUseState from "./Hooks/DemoHookUseState";
-import DemoHookUseEffect from "./Hooks/DemoHookUseEffect";
-import DemoJSS from "./JSS/DemoJSS/DemoJSS";
-import ToDoList from "./JSS/ExStyledComponent/ToDoList/ToDoList";
-import DemoTheme from "./JSS/Theme/DemoTheme";
-import LifeCycleReact from "./LifeCycleReact/LifeCycleReact";
-import logo from "./logo.svg";
-import DemoHookUseCallback from "./Hooks/DemoHookUseCallback";
-import DemoHookUseMemo from "./Hooks/DemoHookUseMemo";
-import DemoUseRef from "./Hooks/DemoUseRef";
-import DemoUseReducer from "./Hooks/DemoUseReducer";
-import DemoUseContext from "./Hooks/DemoUseContext";
-import ContextProvider from "./Hooks/Context/ContextProvider";
-import DemoReduxApp from "./Hooks/DemoReduxApp";
-import DemoUseSpring from "./Hooks/DemoUseSpring";
-import DemoUseSpring2 from "./Hooks/DemoUseSpring2";
-import DemoUseSpring3 from "./Hooks/DemoUseSpring3";
-import DemoUseTrail from "./Hooks/DemoUseTrail";
-import DemoUseTransition from "./Hooks/DemoUseTransition";
-import DemoUseChain from "./Hooks/DemoUseChain";
+
 import BaiTapGameBauCua from "./Exercise/GameBauCua/BaiTapGameBauCua";
 
 function App() {
     return (
         <div className="App">
-            {/* <UserProfile/> */}
-            {/* <DemoJSS/> */}
-            {/* <DemoTheme/> */}
-            {/* <ToDoList/> */}
-            {/* <LifeCycleReact/> */}
-            {/* <DemoHookUseState/> */}
-            {/* <DemoHookUseEffect/> */}
-            {/* <DemoHookUseCallback/> */}
-            {/* <DemoHookUseMemo/> */}
-            {/* <DemoUseRef/> */}
-            {/* <DemoUseReducer/> */}
-            {/* <ContextProvider> */}
-                {/* <DemoUseContext /> */}
-            {/* </ContextProvider> */}
-            {/* <DemoReduxApp/> */}
-            {/* <DemoUseSpring/> */}
-            {/* <DemoUseSpring2/> */}
-            {/* <DemoUseSpring3/> */}
-            {/* <DemoUseTrail/> */}
-            {/* <DemoUseTransition/> */}
-            {/* <DemoUseChain/> */}
             <BaiTapGameBauCua/>
         </div>
     );
